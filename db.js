cf0d1ac12ae6a944de3247fc9f794dc1a2fccc9a
rafifou96 <rapamar@g.jct.ac.il>
2020-06-17 01:29:39 +0300

    connexion

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
  
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -9,7 +8,10 @@ const dbName = 'myproject';
  
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
+    if (err) {
+        console.log("Error", err);
+        return;
+    }
   console.log("Connected successfully to server");
  
   const db = client.db(dbName);
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const db = require('./db');
+
 if (process.argv[2] == 'list') { 
   console.log('list of disk');
 } else if (process.argv[2] == 'add') { 
