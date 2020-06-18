451fa0877c6732cafdde5582bd25cb8d1876daf9
rafifou96 <rapamar@g.jct.ac.il>
2020-06-18 19:49:27 +0300

    version2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const db = require('./db');
-
 const diskServices = require('./services/diskService');
+const prompts = require('prompts');
+const shopService = require('./Services/shopService');
 
 const start = async() => {
 
@@ -13,8 +14,11 @@ const start = async() => {
         qte: 5
     };
 
-  // await diskServices.add(disk);
-   await diskServices.delete("5eeb2d1cd059fc324cbe637c");
+//    await diskServices.add(disk);
+//    await diskServices.delete("5eeb2d1cd059fc324cbe637c");
+
+await shopService.run();
+
 }
 
 start();
--- services/diskService.js
+++ services/diskService.js
@@ -14,31 +14,41 @@ const addDisk = async(diskToAdd) =>{
 const updateDisk = async(diskToUpdate) =>{
     try{
         console.log(diskToUpdate);
-        const result = await diskModel.findByIdAndUpdate(diskToUpdate._id,diskToUpdate);
+        const result = await diskModel.findByIdAndUpdate(diskToUpdate._id,diskToUpdate,);
         return result;
     }catch(e){
         console.log('Failed to update this disk.');
     }
 }
  
-// //Get all disks
-// const getAllDisks = async(query :{}= {} , lean :Boolean = false) =>{
-//     try{
-//         const results = lean ? await diskModel.find(query).lean() : await diskModel.find() ??????
-//         return results;
-//     }catch(e){
-//         console.log('Failed to get all disks.');
-//     }
-// }
+//Get all disks
+const getAllDisk = async (query = {}, lean = false) => {
+    try {
+        const results = lean ? await diskModel.find(query).lean() :  await diskModel.find(query);
+        return results;
+    } catch (e) {
+        console.log(`Cannot get all theses disk.`);
+    }
+};
 
 //Delete a disk 
 const deleteDiskById = async(id)=>{
     try{
-        const result = await diskModel.findByIdAndDelete(id);
+        const result = await diskModel.findByIdAndDelete(id);  
         console.log('disk delete from the DB');
     }catch(e){
         console.log('failed to deleted disk');
     }
 }
 
-module.exports = { add:addDisk, update:updateDisk, delete:deleteDiskById };
+//Get disk by id 
+const getDiskById = async (id) => {
+    try {
+        const result = await diskModel.findOne({_id: id});
+        return result;
+    } catch (e) {
+        console.log(`Cannot get this disk.`);
+    }
+};
+
+module.exports = { add:addDisk, update:updateDisk, getAll:getAllDisk, delete:deleteDiskById, getDisk:getDiskById  };
